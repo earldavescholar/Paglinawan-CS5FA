822b01ee52597d69b6ba1b318e26385e54b2aad8
earldavescholar <epaglinawan@crc.pshs.edu.ph>
2023-08-30 15:22:39 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+name = "Dave"
+age = 10
+hobby = "Studying, playing, and singing"
+address = "Butuan"
+height = 190
+weight = 60
+favBrand = "Nike"
+hairType = "Short and Straight"
+favPlace = "Park"
+phoneBrand = "IPhone 14 pro"
+
+print("Hello! ")
+userName = input("What is your name? ")
+print("Oh nice to meet you,", userName )
+print("My name is ", name)
+userAge = input("How old are you now? ")
+print("Cool:) I also have", userAge, "years old friends and they are good friends to me even if I'm at age of", age)
+userHobby = input("What are the things you like to do? ")
+print("Woah!", userHobby, "is fun but i like", hobby, "more")
+userAddress = input("I'm from " + address + ", Where are you from? ")
+print("I really like to go to", userAddress, "XD.")
+print("I hope i can see you there")
+print("I'm ", height, "cm tall and my weight is", weight, "kg")
+favBrand = input("I really like the brand 'Nike', How about you? ")
+print("Great! Btw my hair type is", hairType, "and I love walking in the", favPlace)
+print("My phone is", phoneBrand)
+userPhone = input("how about you? ")
+print("Cool! Nice talking with you! byebye:))")
